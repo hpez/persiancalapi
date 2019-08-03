20132ee692bdb999a63b7e9f0ddb8ee959509ef5
Hassan Pezeshk <hpez58@gmail.com>
2019-08-03 22:08:01 +0430

    Tests fixed

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -8,28 +8,28 @@ class BasicTest extends TestCase
 {
     public function testJalaliHoliday()
     {
-        $output = $this->get('/jalali/1398/01/01');
+        $output = $this->get('jalali/1398/01/01');
 
         $this->assertEquals($output->json('is_holiday'), true);
     }
 
     public function testJalaliNonHoliday()
     {
-        $output = $this->get('/jalali/1398/05/07');
+        $output = $this->get('jalali/1398/05/07');
 
         $this->assertEquals($output->json('is_holiday'), false);
     }
 
     public function testGregorianHoliday()
     {
-        $output = $this->get('/gregorian/2019/08/12');
+        $output = $this->get('gregorian/2019/08/12');
 
         $this->assertEquals($output->json('is_holiday'), true);
     }
 
     public function testGregorianNonHoliday()
     {
-        $output = $this->get('/gregorian/2019/08/11');
+        $output = $this->get('gregorian/2019/08/11');
 
         $this->assertEquals($output->json('is_holiday'), false);
     }
