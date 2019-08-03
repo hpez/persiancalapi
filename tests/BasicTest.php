3ae424a8ac0134df48dd5e028cda052ea7beec1a
Hassan Pezeshk <hpez58@gmail.com>
2019-08-03 21:46:26 +0430

    Tests fixed

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -8,77 +8,29 @@ class BasicTest extends TestCase
 {
     public function testJalaliHoliday()
     {
-        $handle = curl_init();
+        $output = $this->get('/jalali/1398/01/01');
 
-        $url = "https://persiancalapi.ir/jalali/1398/01/01";
-
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-
-        $output = curl_exec($handle);
-
-        curl_close($handle);
-
-        $output = json_decode($output);
-
-        $this->assertEquals($output->is_holiday, true);
+        $this->assertEquals($output->json('is_holiday'), true);
     }
 
     public function testJalaliNonHoliday()
     {
-        $handle = curl_init();
+        $output = $this->get('/jalali/1398/05/07');
 
-        $url = "https://persiancalapi.ir/jalali/1398/05/07";
-
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-
-        $output = curl_exec($handle);
-
-        curl_close($handle);
-
-        $output = json_decode($output);
-
-        $this->assertEquals($output->is_holiday, false);
+        $this->assertEquals($output->json('is_holiday'), false);
     }
 
     public function testGregorianHoliday()
     {
-        $handle = curl_init();
+        $output = $this->get('/gregorian/2019/08/12');
 
-        $url = "https://persiancalapi.ir/gregorian/2019/08/12";
-
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-
-        $output = curl_exec($handle);
-
-        curl_close($handle);
-
-        $output = json_decode($output);
-
-        $this->assertEquals($output->is_holiday, true);
+        $this->assertEquals($output->json('is_holiday'), true);
     }
 
     public function testGregorianNonHoliday()
     {
-        $handle = curl_init();
+        $output = $this->get('/gregorian/2019/08/11');
 
-        $url = "https://persiancalapi.ir/gregorian/2019/08/11";
-
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-
-        $output = curl_exec($handle);
-
-        curl_close($handle);
-
-        $output = json_decode($output);
-
-        $this->assertEquals($output->is_holiday, false);
+        $this->assertEquals($output->json('is_holiday'), false);
     }
 }
